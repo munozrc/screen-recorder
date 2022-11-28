842ef6a4b07a61973bd76359557293a5a7f8608f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-11-27 22:31:24 -0500

    feat: Add global styles css

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./app"
 
-import "./index.css"
+import "./styles/index.css"
 
 const rootElement = document.getElementById("root") as HTMLElement
 const createRoot = ReactDOM.createRoot(rootElement)
