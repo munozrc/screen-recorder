c3344449dc01c69e6c5f509c708d8a29c475aa07
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-12-02 15:31:37 -0500

    fix: Move responsibilities for ScreenPreview

--- src/components/screen-preview/screen-preview.tsx
+++ src/components/screen-preview/screen-preview.tsx
@@ -1,13 +1,30 @@
-import { forwardRef } from "react"
+import { useEffect, useRef } from "react"
 import styles from "./screen-preview.module.css"
 
-const ScreenPreview = forwardRef<HTMLVideoElement>((_, videoElement) => {
+interface ScreenPreviewProps {
+  srcPreview: MediaStream | undefined
+  src: string | undefined
+}
+
+export default function ScreenPreview ({ srcPreview, src }: ScreenPreviewProps) {
+  const videoElement = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    if (!videoElement.current) return
+    videoElement.current.srcObject = srcPreview ?? null
+  }, [srcPreview])
+
   return (
     <div className={styles.container}>
-      <video className={styles.video} ref={videoElement}/>
+      <video
+        className={styles.video}
+        ref={videoElement}
+        src={src}
+        autoPlay={srcPreview !== undefined}
+        muted={srcPreview !== undefined}
+        controls={typeof src === "string"}
+        poster={typeof src === "string" ? undefined : "/no-video.svg"}
+      />
     </div>
   )
-})
-
-ScreenPreview.displayName = "ScreenPreview"
-export default ScreenPreview
+}
